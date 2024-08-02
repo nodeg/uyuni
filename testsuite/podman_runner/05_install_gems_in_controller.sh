aa444ae0aea50f8a3715ff1a1216856b098b1d20
Dominik Gedon <dominik.gedon@suse.com>
2024-08-02 11:27:51 +0200

    QE: Update Ruby from 2.5 to 3.3
    See https://www.ruby-lang.org/en/news/2019/12/12/separation-of-positional-and-keyword-arguments-in-ruby-3-0/
    
    Signed-off-by: Dominik Gedon <dominik.gedon@suse.com>

--- testsuite/podman_runner/05_install_gems_in_controller.sh
+++ testsuite/podman_runner/05_install_gems_in_controller.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 set -xe
-sudo -i podman exec controller bash -c "cd /testsuite && bundle.ruby2.5 install --gemfile Gemfile"
+sudo -i podman exec controller bash -c 'export GEM_PATH="/usr/lib64/ruby/gems/3.3.0"'
+sudo -i podman exec controller bash -c "cd /testsuite && bundle.ruby3.3 install --gemfile Gemfile --verbose"
+sudo -i podman exec controller bash -c "gem env"
+sudo -i podman exec controller bash -c "gem list"
+
+# Necessary due to building Twopence from source
+sudo -i podman exec controller bash -c "ldconfig -p | grep twopence"
+sudo -i podman exec controller bash -c "ln -sf /usr/lib64/ruby/gems/3.3.0/gems/twopence-0.4.2/lib/twopence.so /usr/lib64/ruby/gems/3.3.0/gems/twopence-0.4.2/lib/twopence.so.0"
+sudo -i podman exec controller bash -c "ldconfig"
+sudo -i podman exec controller bash -c "ldconfig -p | grep twopence"
